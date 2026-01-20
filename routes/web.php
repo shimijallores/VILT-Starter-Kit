662ab3dd9a60738e9c1f41162777cd77f5495d00
zielww <118595018+zielww@users.noreply.github.com>
2026-01-20 12:56:04 +0800

    added pint and rector

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function (): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View {
     return view('welcome');
 });
